c0a20f07466ad60b011f2cc9d7fe02bdd6baec43
hotfireeagle <hotfireeagle@163.com>
2020-02-21 11:29:12 +0800

    update:add ormconfig init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
+createConnection().then(async connection => {
+
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+
+    const users = await connection.manager.find(User);
+
+
+}).catch(err => {
+  throw new Error(err)
+});
